71652ce262cc696393453e55ed9a3d6c48dea0a0
Marcus Klein <marcus.klein@open-xchange.com>
2018-11-02 14:55:09 +0100

    receive messages

--- nest/nest.js
+++ nest/nest.js
@@ -1,13 +1,13 @@
 module.exports = function(RED) {
-    RED.log.info("Nest dashboard element " + require('../package.json').version)
-    var ui = undefined;
-    function NEST(config) {
+    RED.log.info("Nest dashboard element " + require("../package.json").version)
+    RED.nodes.registerType("nest", nest);
+
+    function nest(config) {
+        var node = this;
+        RED.nodes.createNode(node, config);
+
         try {
-            var node = this;
-            if (ui === undefined) {
-                ui = RED.require("node-red-dashboard")(RED);
-            }
-            RED.nodes.createNode(node, config);
+            var ui = RED.require("node-red-dashboard")(RED);
             var done = ui.addWidget({
                 node: node,
                 format: "<div>Lala</div>",
@@ -20,7 +20,12 @@ module.exports = function(RED) {
             this.error("Kaputt");
             node.error("Hit an error", e);
         }
+
+        node.on('input', function(msg) {
+            node.log("Received: " + msg.payload);
+            node.status({fill: "green", shape: "ring", text: msg.payload});
+        });
+
         node.on("close", done);
     }
-    RED.nodes.registerType("nest", NEST);
 }
